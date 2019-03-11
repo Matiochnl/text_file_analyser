02878401c13fc833c6c068d2cf37259b0be68c28
Szymon Pluta <satanthesavior>
2019-03-11 13:17:45 +0100

    Add character counting function

---
+++ analyzer.py
@@ -0,0 +1,12 @@
+import data_manager
+
+
+def character_count():
+    count = 0
+    for elem in data_manager.open_file("resources/input.txt"):
+        for el in elem:
+            if el == " ":
+                count = count
+            else:
+                count += 1
+        return count
